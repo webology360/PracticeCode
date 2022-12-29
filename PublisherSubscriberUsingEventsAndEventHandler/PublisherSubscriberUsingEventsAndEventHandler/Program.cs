371c901accc9b8b7ac3303678de471f9f75dc5d8
galibgaurav <galibgaurav@gmail.com>
2022-12-29 12:46:31 +0530

    Event publisher subscriber using inbuilt event handler delegate in c#
    Event publisher subscriber using inbuilt event handler delegate in c#

---
+++ PublisherSubscriberUsingEventsAndEventHandler/PublisherSubscriberUsingEventsAndEventHandler/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EventDelegatePublisherSubcriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var video = new Video() { videoName = "video1", videoPath = "https://www.abc.com/video1.mp4" };
+            var videoEncoder = new VideoEncoder(); //publisher
+            var mailService = new MailService(); //subscriber
+            
+            //Subscriber to subcribe to the publisher,
+            //Great thing to note is thar both subscriber and publisher
+            //are not tightly coupled.
+
+            videoEncoder.VideoEncoded += mailService.OnVideoEncoded;
+
+            videoEncoder.Encode(video);
+            Console.Read();
+        }
+    }
+}
---
+++ PublisherSubscriberUsingEventsAndEventHandler/PublisherSubscriberUsingEventsAndEventHandler/VideoEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventDelegatePublisherSubcriber
+{
+    //Video encorder is a publisher for a event "VideoEncoded"
+    //which will be rasied when video is encoded 
+
+    public class VideoEventArgs: EventArgs
+    {
+        public string videoName;
+    }
+}
